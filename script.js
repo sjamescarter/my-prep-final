278eeb960436ae9bde32136352066c34684c58f4
sjamescarter <stephenjamescarter@gmail.com>
2022-10-28 14:05:17 -0500

    added text to paragraphs

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
 const menu1 = document.getElementById('menu1');
 const menu2 = document.getElementById("menu2");
 const menu3 = document.getElementById("menu3");
